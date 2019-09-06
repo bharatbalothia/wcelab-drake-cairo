24e0cdf0ff9c631707abafb3862e27c5b8efdf28
Rudramurthy Tumkur <rtumkur@us.ibm.com>
2019-09-06 11:26:05 -0400

    initial directory structure for a maven project.

---
+++ cairo-drake/src/main/java/com/ibm/oms/cairo_drake/App.java
@@ -0,0 +1,13 @@
+package com.ibm.oms.cairo_drake;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
