76214f9a5189c8280ec5a5174b8cb8771cd91dbd
Rudramurthy Tumkur Prabhashankar <rtumkur@us.ibm.com>
2019-09-12 10:30:00 -0400

    uncommit few lines

--- cairo-drake/runtime/extensions/global/runScripts.sh
+++ cairo-drake/runtime/extensions/global/runScripts.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-
-cd /opt/ssfs/runtime/bin && ./deployer.sh -t resourcejar
-cd /opt/ssfs/runtime/bin && ./deployer.sh -t entitydeployer
+sed -i 's/NO_DBVERIFY=false/NO_DBVERIFY=true/g' /opt/ssfs/runtime/properties/sandbox.cfg
+#cd /opt/ssfs/runtime/bin && ./deployer.sh -t resourcejar
+#cd /opt/ssfs/runtime/bin && ./deployer.sh -t entitydeployer
 cd /opt/ssfs/runtime/docker-samples/imagebuild && ./generateImages.sh --MODE=agent
-cd /opt/ssfs/runtime/docker-samples/imagebuild && ./generateImages.sh --MODE=app
+#cd /opt/ssfs/runtime/docker-samples/imagebuild && ./generateImages.sh --MODE=app
 
 
