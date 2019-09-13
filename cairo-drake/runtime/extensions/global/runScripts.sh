2574c208d03c172703beeee7945b128942213601
Rudramurthy Tumkur Prabhashankar <rtumkur@us.ibm.com>
2019-09-12 22:38:54 -0400

    added command to run sed fix script

--- cairo-drake/runtime/extensions/global/runScripts.sh
+++ cairo-drake/runtime/extensions/global/runScripts.sh
@@ -3,6 +3,7 @@
 sed -i 's/NO_DBVERIFY=false/NO_DBVERIFY=true/g' /opt/ssfs/runtime/properties/sandbox.cfg
 #cd /opt/ssfs/runtime/bin && ./deployer.sh -t resourcejar
 #cd /opt/ssfs/runtime/bin && ./deployer.sh -t entitydeployer
+cd /opt/ssfs/runtime && ./runSed.sh 
 cd /opt/ssfs/runtime/docker-samples/imagebuild && ./generateImages.sh --MODE=agent
 #cd /opt/ssfs/runtime/docker-samples/imagebuild && ./generateImages.sh --MODE=app
 
