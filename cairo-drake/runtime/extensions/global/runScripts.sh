b0fb9bb3d4ead840bb51bcd4027d300febe65ed5
Rudramurthy Tumkur Prabhashankar <rtumkur@us.ibm.com>
2019-09-16 22:27:43 -0400

    added agent_repo and agent_tag inputs

--- cairo-drake/runtime/extensions/global/runScripts.sh
+++ cairo-drake/runtime/extensions/global/runScripts.sh
@@ -4,7 +4,8 @@ sed -i 's/NO_DBVERIFY=false/NO_DBVERIFY=true/g' /opt/ssfs/runtime/properties/san
 #cd /opt/ssfs/runtime/bin && ./deployer.sh -t resourcejar
 #cd /opt/ssfs/runtime/bin && ./deployer.sh -t entitydeployer
 cd /opt/ssfs/runtime && ./runSed.sh 
-cd /opt/ssfs/runtime/docker-samples/imagebuild && ./generateImages.sh --MODE=agent
+ln -s 
+cd /opt/ssfs/runtime/docker-samples/imagebuild && ./generateImages.sh --MODE=agent --AGENT_REPO=${AGENT_REPO} --AGENT_TAG=${AGENT_TAG} 
 #cd /opt/ssfs/runtime/docker-samples/imagebuild && ./generateImages.sh --MODE=app
 
 
