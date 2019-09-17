208ddb42a0f272e624cd220e1f01280ad31ef3b4
Rudramurthy Tumkur Prabhashankar <rtumkur@us.ibm.com>
2019-09-16 22:53:38 -0400

    added code to push.

--- cairo-drake/runtime/extensions/global/runScripts.sh
+++ cairo-drake/runtime/extensions/global/runScripts.sh
@@ -7,7 +7,9 @@ cd /opt/ssfs/runtime && ./runSed.sh
 mkdir -p /home/omsuser/.docker
 ln -s /var/run/secrets/openshift.io/push /home/omsuser/.docker/config.json
 
-cd /opt/ssfs/runtime/docker-samples/imagebuild && ./generateImages.sh --MODE=agent --AGENT_REPO=${AGENT_REPO} --AGENT_TAG=${AGENT_TAG} 
+echo "Agent Repo : ${AGENT_REPO}  Agent Tag: ${AGENT_TAG}"
+cd /opt/ssfs/runtime/docker-samples/imagebuild && ./generateImages.sh --MODE=agent --AGENT_REPO=oms-agent-custom --AGENT_TAG=latest 
 #cd /opt/ssfs/runtime/docker-samples/imagebuild && ./generateImages.sh --MODE=app
+#buildah --storage-driver vfs push oms-agent-custom:latest docker:${AGENT_REPO}:${AGENT_TAG}
 
 
