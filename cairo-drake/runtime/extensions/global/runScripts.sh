98bf32d9504dd81d8520aac47f859275994b2c44
Rudramurthy Tumkur Prabhashankar <rtumkur@us.ibm.com>
2019-09-16 22:31:57 -0400

    linked the push secret

--- cairo-drake/runtime/extensions/global/runScripts.sh
+++ cairo-drake/runtime/extensions/global/runScripts.sh
@@ -4,7 +4,9 @@ sed -i 's/NO_DBVERIFY=false/NO_DBVERIFY=true/g' /opt/ssfs/runtime/properties/san
 #cd /opt/ssfs/runtime/bin && ./deployer.sh -t resourcejar
 #cd /opt/ssfs/runtime/bin && ./deployer.sh -t entitydeployer
 cd /opt/ssfs/runtime && ./runSed.sh 
-ln -s 
+mkdir -p /home/omsuser/.docker
+ln -s /var/run/secrets/openshift.io/push /home/omsuser/.docker/config.json
+
 cd /opt/ssfs/runtime/docker-samples/imagebuild && ./generateImages.sh --MODE=agent --AGENT_REPO=${AGENT_REPO} --AGENT_TAG=${AGENT_TAG} 
 #cd /opt/ssfs/runtime/docker-samples/imagebuild && ./generateImages.sh --MODE=app
 
