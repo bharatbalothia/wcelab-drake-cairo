af3888b5be9b5ce3b3056079a1836e8afc393aae
Rudramurthy Tumkur <rtumkur@us.ibm.com>
2019-10-02 23:18:48 -0400

    changes to build.sh. uploading latest platform_afc.jar

--- build.sh
+++ build.sh
@@ -15,19 +15,35 @@ echo 'Done installing extensions'
 
 echo 'Starting the Image Generation scripts'
 
-BUILD_MODE=${BUILD_MODE:-"all"}
-cd /opt/ssfs/runtime/docker-samples/imagebuild && ./generateImages.sh --MODE=${MODE} 
 
-echo 'Done generated Images...'
+
+
 
 cp /var/run/secrets/openshift.io/push/.dockercfg /tmp
 (echo "{ \"auths\": " ; cat /var/run/secrets/openshift.io/push/.dockercfg ; echo "}") > /tmp/.dockercfg
 
 if [ $BUILD_MODE = "agent" ]
 then 
+    cd /opt/ssfs/runtime/docker-samples/imagebuild && ./generateImages.sh --MODE=agent
+    buildah push --authfile /tmp/.dockercfg --cert-dir /var/run/secrets/kubernetes.io/serviceaccount localhost:oms-agent:10.0 docker://${OUTPUT_REGISTRY}:${OUTPUT_IMAGE}
+elif [ $BUILD_MODE = "agent" ]
+then
+    cd /opt/ssfs/runtime/docker-samples/imagebuild && ./generateImages.sh --MODE=app
+    buildah push --authfile /tmp/.dockercfg --cert-dir /var/run/secrets/kubernetes.io/serviceaccount localhost:oms-app:10.0 docker://${OUTPUT_REGISTRY}:${OUTPUT_IMAGE}
+elif [ $BUILD_MODE = "base" ]
+then
+    cd /opt/ssfs/runtime/docker-samples/imagebuild && ./generateImages.sh --MODE=base
+    buildah push --authfile /tmp/.dockercfg --cert-dir /var/run/secrets/kubernetes.io/serviceaccount localhost:oms-base:10.0 docker://${OUTPUT_REGISTRY}:${OUTPUT_IMAGE}
+elif [ $BUILD_MODE = "cdt_import" ]
+then
+    export SOURCE_DB=${SOURCE_DB:-"MC_XML"}
+    export SOURCE_PASSWORD=${SOURCE_PASSWORD:-""}
+    sed -i 's/SOURCE_DB/SOURCE_DB=${SOURCE_DB}/g' /opt/ssfs/runtime/bin/cdtshell.sh
+    sed -i 's/SOURCE_PASSWORD/SOURCE_PASSWORD=${SOURCE_PASSWORD}/g' /opt/ssfs/runtime/bin/cdtshell.sh
+    sed -i 's/TARGET_DB/TARGET_DB=${TARGET_DB}/g' /opt/ssfs/runtime/bin/cdtshell.sh
+    sed -i 's/TARGET_PASSWORD/TARGET_PASSWORD=${TARGET_PASSWORD}/g' /opt/ssfs/runtime/bin/cdtshell.sh
+    cd /opt/ssfs/runtime/bin && ./cdtshell.sh 
+fi 
 
-
-fi
-buildah push --authfile /tmp/.dockercfg --cert-dir /var/run/secrets/kubernetes.io/serviceaccount localhost:oms-agent:10.0 docker://${OUTPUT_REGISTRY}:${OUTPUT_IMAGE}
 
 
